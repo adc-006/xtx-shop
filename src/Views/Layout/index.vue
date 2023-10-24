e6eb74809cddc8d24223d2419ecbbd5b623ca347
linyc <1256890952@qq.com>
2023-10-24 22:38:49 +0800

    update: 项目初始化完成

---
+++ src/Views/Layout/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import LayoutNav from '@/Views/Layout/components/LayoutNav.vue'
+import LayoutHeader from '@/Views/Layout/components/LayoutHeader.vue'
+import LayoutFooter from '@/Views/Layout/components/LayoutFooter.vue'
+</script>
+
+<template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      component: () => import('@/Views/Layout/index.vue'),
+      children: [
+        {
+          path: '',
+          component: () => import('@/Views/Home/index.vue')
+        },
+        {
+          path: 'category',
+          component: () => import('@/Views/Category/index.vue')
+        },
+      ]
+    },
+    {
+      path: '/login',
+      component: () => import('@/Views/Login/index.vue')
+    }
   ]
 })
 
